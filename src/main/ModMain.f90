a1f6a55252009cb71c578840667deceb706973cc
Aaron Ridley <ridley@umich.edu>
2024-10-09 14:46:09 -0400

    FEAT: store variables from main and output

---
+++ src/main/ModMain.f90
@@ -0,0 +1,111 @@
+
+Module ModMain
+
+  use ModKind
+  
+  character (len=100) :: outputDir = 'IE/output'
+  character (len=100) :: filename = 'test.bin'
+  integer :: LunMainOutput = 60
+  logical :: isInitialized = .false.
+
+  integer :: nTimes = 12, iT
+  real (kind = Real8_) :: currentTime = -1.0
+  real (kind = Real8_) :: startTime = -1.0
+  real (kind = Real8_) :: endTime = -1.0
+
+  real :: imfBx, imfBy, imfBz
+  real :: ae, au, al, hp
+  real :: swV, swN
+  
+  integer :: nLons = 25
+  integer :: nLats = 30
+  real :: minLat = 60.0
+  logical :: isGeographic = .false.
+  real, allocatable :: magLats(:, :)
+  real, allocatable :: magLons(:, :)
+  real, allocatable :: magMlts(:, :)
+
+  real, allocatable :: geoLats(:, :)
+  real, allocatable :: geoLons(:, :)
+  
+  real, allocatable :: gseX(:, :)
+  real, allocatable :: gseY(:, :)
+  real, allocatable :: gseZ(:, :)
+
+  real, allocatable :: potential(:, :)
+  real, allocatable :: eDiffuseAvee(:, :)
+  real, allocatable :: eDiffuseEflux(:, :)
+  real, allocatable :: iDiffuseAvee(:, :)
+  real, allocatable :: iDiffuseEflux(:, :)
+  real, allocatable :: eMonoAvee(:, :)
+  real, allocatable :: eMonoEflux(:, :)
+  real, allocatable :: eWaveAvee(:, :)
+  real, allocatable :: eWaveEflux(:, :)
+  
+contains
+
+  ! ----------------------------------------------------------------
+  ! Allocate all variables
+  ! ----------------------------------------------------------------
+
+  subroutine allocate_all_variables
+
+    if (allocated(magLats)) return
+
+    allocate(magLats(nLons, nLats))
+    allocate(magMlts(nLons, nLats))
+
+    allocate(potential(nLons, nLats))
+    allocate(eDiffuseAvee(nLons, nLats))
+    allocate(eDiffuseEflux(nLons, nLats))
+    allocate(iDiffuseAvee(nLons, nLats))
+    allocate(iDiffuseEflux(nLons, nLats))
+    allocate(eMonoAvee(nLons, nLats))
+    allocate(eMonoEflux(nLons, nLats))
+    allocate(eWaveAvee(nLons, nLats))
+    allocate(eWaveEflux(nLons, nLats))
+
+    if (isGeographic) then
+       allocate(geoLats(nLons, nLats))
+       allocate(geoLons(nLons, nLats))
+       allocate(magLons(nLons, nLats))
+       allocate(gseX(nLons, nLats))
+       allocate(gseY(nLons, nLats))
+       allocate(gseZ(nLons, nLats))
+    endif
+
+  end subroutine allocate_all_variables
+
+  ! ----------------------------------------------------------------
+  ! Deallocate all variables
+  ! ----------------------------------------------------------------
+
+  subroutine deallocate_all_variables
+
+    if (.not. allocated(magLats)) return
+
+    deallocate(magLats)
+    deallocate(magMlts)
+
+    deallocate(potential)
+    deallocate(eDiffuseAvee)
+    deallocate(eDiffuseEflux)
+    deallocate(iDiffuseAvee)
+    deallocate(iDiffuseEflux)
+    deallocate(eMonoAvee)
+    deallocate(eMonoEflux)
+    deallocate(eWaveAvee)
+    deallocate(eWaveEflux)
+
+    if (isGeographic) then
+       deallocate(geoLats)
+       deallocate(geoLons)
+       deallocate(magLons)
+       deallocate(gseX)
+       deallocate(gseY)
+       deallocate(gseZ)
+    endif
+
+  end subroutine deallocate_all_variables
+  
+end Module ModMain
---
+++ src/main/output.f90
@@ -0,0 +1,83 @@
+
+subroutine output_ie(iError)
+
+  use ModMain
+  use ModErrors
+  use ModTimeConvert
+  
+  implicit none
+
+  integer, intent(out) :: iError
+  integer, dimension(7) :: itime
+
+  real :: dst, dstCalc
+  integer :: i, j
+  
+  iError = 0
+
+  call time_real_to_int(currentTime, itime)
+  
+  if (.not. isInitialized) then
+
+     write(*,*) ' => opening file : ', trim(filename)
+     open(LunMainOutput, &
+          file = trim(filename), &
+          status = 'unknown', &
+          form = 'UNFORMATTED', &
+          iostat = iError)
+
+     if (iError /= 0) then
+        call set_error("Error opening output file: ")
+        call set_error(filename)
+        return
+     endif
+
+     write(LunMainOutput) nLats, nLons, nTimes
+     if (isGeographic) then
+        write(LunMainOutput) 90.0 - geoLats(1,:)
+        write(LunMainOutput) geoLons(:,1)/15.0
+        write(LunMainOutput) 5
+        write(LunMainOutput) 'Electric Potential (kV)       '
+        write(LunMainOutput) 'Auroral Energy Flux (mW/m2)   '
+        write(LunMainOutput) 'Auroral Mean Energy (keV)     '
+        write(LunMainOutput) 'Magnetic Latitude (deg)       '
+        write(LunMainOutput) 'Magnetic Local Time (hours)   '
+     else
+        write(LunMainOutput) 90.0 - magLats(1,:)
+        write(LunMainOutput) magMlts(:,1)
+        write(LunMainOutput) 3
+        write(LunMainOutput) 'Electric Potential (kV)       '
+        write(LunMainOutput) 'Auroral Energy Flux (mW/m2)   '
+        write(LunMainOutput) 'Auroral Mean Energy (keV)     '
+        
+     endif
+
+     isInitialized = .true.
+     
+  endif
+
+  write(LunMainOutput) iT, iTime(1), iTime(2), iTime(3), iTime(4), iTime(5)
+
+  dst = 0.0
+  dstCalc = 0.0
+  write(LunMainOutput) swV, imfBx, imfBy, imfBz, 0.0, &
+       ae, au, al, dst, dstCalc, hp, 0.0, &
+       maxval(potential) - minval(potential)
+  
+  write(LunMainOutput) &
+       ((potential(j, i), j = 1, nLons), i = 1, nLats)
+  write(LunMainOutput) &
+       ((eDiffuseEFlux(j, i), j = 1, nLons), i = 1, nLats) 
+  write(LunMainOutput) &
+       ((eDiffuseAveE(j, i), j = 1, nLons), i = 1, nLats) 
+
+  if (isGeographic) then
+     write(LunMainOutput) &
+          ((magLats(j, i), j = 1, nLons), i = 1, nLats)
+     write(LunMainOutput) &
+          ((magMlts(j, i), j = 1, nLons), i = 1, nLats)
+  endif
+  
+  return
+
+end subroutine output_ie
